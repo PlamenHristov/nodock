85aec016daff89fa1be81dabd10dd89cdd44bd21
Plamen Hristov <hristov.reading@gmail.com>
2018-01-16 22:03:37 +0200

    Changed docker-compose file. All should be well now :)

--- nginx/scripts/build-nginx.sh
+++ nginx/scripts/build-nginx.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for conf in /etc/nginx/templates/*.conf; do
+for conf in "/etc/nginx/templates/"*.conf; do
     mv $conf "/etc/nginx/sites-available/"$(basename $conf) > /dev/null
 done
 
-for template in /etc/nginx/templates/*.template; do
+for template in "/etc/nginx/templates"/*.template; do
     envsubst < $template > "/etc/nginx/sites-available/"$(basename $template)".conf"
 done
 
